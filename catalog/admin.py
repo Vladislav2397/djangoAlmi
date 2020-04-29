cc87423e479bcbb577a76c54dab09130bff13f1d
GlaDiatoR <valdiszz53@gmail.com>
2020-04-29 20:25:21 +0300

    Create catalog and base page. Add some images

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['subcategory']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = [
+        ('title', 'category'),
+        'description',
+        ('body', 'producer', 'price'),
+        ('image', 'analog')
+    ]
+    list_display = ['title', 'category', 'price']
--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,17 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField('Название', max_length=150)
-    subcategory = models.ManyToManyField('self', verbose_name='Подкатегории')
+    subcategory = models.ManyToManyField(
+        'self', verbose_name='Подкатегории', blank=True
+    )
+
+    def get_subcategories(self):
+        return self.subcategory.all()
+
+    def is_main_category(self):
+        if self.subcategory.get(id=1):
+            return True
+        return False
 
     def __str__(self):
         return f'{self.title}'
@@ -15,7 +25,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField('Название', max_length=200, null=True)
-    description = models.TextField('Описание')
+    description = models.TextField(
+        'Описание', default='Для данного товара пока нет описания.'
+    )
 
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, null=True,
@@ -24,10 +36,10 @@ class Product(models.Model):
 
     body = models.CharField('Корпус', max_length=30, null=True)
     producer = models.CharField('Производитель', max_length=30, null=True)
-    price = models.PositiveSmallIntegerField()
+    price = models.FloatField('Цена')
 
     image = models.ImageField(
-        'Изображение', upload_to=f'products/{str(category)}/'
+        'Изображение', upload_to='products/', blank=True
     )
 
     analog = models.ManyToManyField(
